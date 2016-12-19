ceaf548764c97c6fe7eac2c5395cfb9b33623ab7
chaoyang <mineor@126.com>
2016-12-19 15:58:39 +0800

    node define

---
+++ java/top/mineor/linkedlist/Node.java
@@ -0,0 +1,13 @@
+package top.mineor.linkedlist;
+
+/**
+ * Created by mineor on 2016/12/19.
+ */
+public class Node {
+    int val;
+    Node next;
+    public Node(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
